6950a2eec7c80bbef14d543b381610c033e5fcca
Allie Soldau <alliesoldau@gmail.com>
2023-01-11 11:48:13 -0500

    complete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,8 @@
-class ReviewsController < ApplicationController
+  class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    render json: reviews, include: :dog_house
   end
 
 end
